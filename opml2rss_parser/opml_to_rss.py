ed0ca45ed5c5e5081045e5e20bc71989c0c44e00
Omkar Kulkarni <omkisaik@gmail.com>
2025-11-13 21:07:08 -0800

    finished opml_to_rss, now have a list of 258 rss which can be parsed using existing scrapers

--- opml2rss_parser/opml_to_rss.py
+++ opml2rss_parser/opml_to_rss.py
@@ -4,7 +4,6 @@ from pprint import pprint
 from opml_parser import get_countries_with_opml
 
 def parse_opml(url):
-    print(f"Fetching OPML: {url}")
     response = requests.get(url)
     response.raise_for_status()
 
@@ -15,23 +14,42 @@ def parse_opml(url):
     # loop through all <outline ... xmlUrl="..."> tags in osml dictionary
     for outline in soup.find_all("outline"):
         if outline.has_attr("xmlUrl") and outline.get("type") == "rss":
-            feed_name = outline.get("title", "Unknown")
+            feed_name = outline.get("title") or outline.get("text") or "Feed Name Unkown"
             feed_url = outline["xmlUrl"]
             rss_urls[feed_name] = feed_url
 
     return rss_urls
 
+def get_all_rss():
+    print("Fetching RSS list for all countries from the OPML")
+    countries_dict = get_countries_with_opml()
+    # pprint(countries_dict)
 
-countries_dict = get_countries_with_opml()
-pprint(countries_dict)
+    full_rss_index = {}
 
-full_rss_index = {}
+    for country, rss_urls in countries_dict.items():
+        print(f"Fetching OPML: {country}") 
+        opml_url = rss_urls["without_category"]  
+        rss_list = parse_opml(opml_url)
 
-for country, rss_urls in countries_dict.items():
-    opml_url = rss_urls["without_category"]   
-    rss_list = parse_opml(opml_url)
+        full_rss_index[country] = rss_list
 
-    full_rss_index[country] = rss_list
+    return full_rss_index
 
-return full_rss_index
+def main():
+    rss_dict = get_all_rss()
+    pprint(rss_dict)
+
+    # Total RSS feeds
+    per_country_counts = {country: len(feeds) for country, feeds in rss_dict.items()}
+    total_feeds = sum(per_country_counts.values())
+
+    pprint(per_country_counts)
+    print("\nTOTAL RSS FEEDS FOUND:", total_feeds)
+    
+
+if __name__ == "__main__":
+    main()
+
+
 
